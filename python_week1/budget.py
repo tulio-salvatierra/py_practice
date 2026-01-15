49f3bf52e51caed68cf2658e004951b2ea7d7feb
Tulio Salvatierra <salvacorp@gmail.com>
2026-01-15 17:23:02 -0600

    Add budget script to calculate total monthly expenses

---
+++ python_week1/budget.py
@@ -0,0 +1,10 @@
+# budget script
+
+rent = 1300
+food = 400
+transport = 200
+entertainment = 150
+
+expenses = rent + food + transport + entertainment
+
+print("Total monthly expenses: $" + str(expenses))
