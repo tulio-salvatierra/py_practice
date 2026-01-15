5d722a5de6b5489fe1563586e680cedbceb574d1
Tulio Salvatierra <salvacorp@gmail.com>
2026-01-15 17:27:31 -0600

    Add countdown script to calculate remaining time until ML course

---
+++ python_week1/countdown.py
@@ -0,0 +1,10 @@
+#countdown script
+
+weeks_left = 8
+days_per_week = 7
+
+total_days = weeks_left * days_per_week
+
+total_hours = total_days * 24
+
+print("Only " + str(total_days) +  " or " + str(total_hours) + " hours left til ML course!")
