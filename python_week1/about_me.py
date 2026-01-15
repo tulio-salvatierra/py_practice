e6d8d1dca8214e85c3475c7e1bf9b88327fe9743
Tulio Salvatierra <salvacorp@gmail.com>
2026-01-15 17:13:12 -0600

    Add initial about_me.py file with personal information variables

---
+++ python_week1/about_me.py
@@ -0,0 +1,10 @@
+# create vaariables
+
+name = "Tulio Salvatierra"
+age = 41
+previous_career = 'Truck Driver'
+excited = True
+
+print("Hello, my name is " + name + ".")
+print("I am " + str(age) + " years old.")
+print("My previous career was as a " + previous_career + ".")
