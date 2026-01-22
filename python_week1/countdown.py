a9405ef4be48254ca8bd7a922701fdbd74d682b4
Tulio Salvatierra <salvacorp@gmail.com>
2026-01-22 16:21:15 -0600

    Add epoch tracking and loss/accuracy display to countdown script; create quiz answers file

--- python_week1/countdown.py
+++ python_week1/countdown.py
@@ -1,5 +1,8 @@
 #countdown script
 
+from tracemalloc import start
+
+
 weeks_left = 8
 days_per_week = 7
 
@@ -8,3 +11,13 @@ total_days = weeks_left * days_per_week
 total_hours = total_days * 24
 
 print("Only " + str(total_days) +  " or " + str(total_hours) + " hours left til ML course!")
+
+
+epoch = 1 
+total_epoch = 100
+loss = 0.456
+accuracy = 0.892
+
+print(f'Epoch {epoch}/{total_epoch}:')
+print(f'Loss: {loss:.3f}')
+print(f'Accuracy: {accuracy:.1%}')
---
+++ python_week1/quiz1 answers.md
@@ -0,0 +1,11 @@
+
+	1.	x = 5
+	2.	float
+	3.	**
+	4.	3
+	5.	#
+	6.	<class ‘bool’>
+	7.	==
+	8.	11
+	9.	2nd_var
+	10.	Display output
