70321c80478236f5d9f174d87fec6c4ec5db948e
Tulio Salvatierra <salvacorp@gmail.com>
2026-02-03 20:31:50 -0600

    Add isStatements.py with various conditional examples and logic

---
+++ python_week4/isStatements.py
@@ -0,0 +1,85 @@
+
+# grade = 85
+
+# if grade >= 60:
+#     print("You passed!")
+#     print("Congratulations!")
+# print("Grade check complete.")
+
+# age = 25
+# score = 95
+# name = "Alice"
+
+# print(age >= 18)
+# print(score == 100)
+# print(name != "Bob")
+
+# if score >= 90:
+#     print("A grade!")
+#     if age <=21:
+#         print("Under 21")
+
+# grade = 75
+# if grade >= 60:
+#     print("PASSED")
+#     status = "passed"
+# else:
+#     print("FAILED")
+#     status = "fail"
+# print(f'Status: {status}')
+
+# score = 87
+# if score >= 90:
+#     letter = 'A'
+# elif score >= 80:
+#     letter = 'B'
+# elif score >= 70:
+#     letter = 'C'
+# elif score >= 60:
+#     letter = 'D'
+# else:
+#     letter = 'F'    
+#     print(f'Score {score} = Grade {letter}')
+
+# age = 25
+# has_license = True
+
+# if age >= 21 and has_license:
+#     print("You can rent a car!")
+# else:
+#     print("You cannot rent a car.")
+
+# is_weekend = True
+# is_holiday = False
+# if is_weekend or is_holiday:
+#     print("sleep in!")
+# else:
+#     print("Set alarm!")
+
+# is_raining = False
+
+# if not is_raining:
+#     print("Go for a walk!")
+# else:
+#     print("Stay inside!")
+
+# gpa = 3.8
+# test_score = 1350
+# has_reccomendation = True
+# if (gpa >= 3.5 and test_score >= 1300) or has_reccomendation:
+#     print("You are accepted!")
+# else:
+#     print("You are not accepted.")
+
+# data = [1,2,3]
+
+# if len(data) > 0:
+#     print("Has Data")
+# if data:
+#     print("Also has data")
+
+a, b = 10, 15
+max_value = a if a > b else b
+print(f'Max value is {max_value}')
+    
+    
